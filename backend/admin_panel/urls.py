14e1ca46e58fe47b9a79f0471827517921cd0981
slvajunior <johnf.foto@gmail.com>
2025-04-02 04:14:56 -0300

    fix: PainelAdministrativo/Dashboard II

--- backend/admin_panel/urls.py
+++ backend/admin_panel/urls.py
@@ -10,10 +10,10 @@ router = DefaultRouter()
 router.register(r'owners', OwnerViewSet)
 router.register(r'saloes', SalaoViewSet)
 
-
+# Remova o registro da DashboardView do router e adicione como uma URL normal
 urlpatterns = [
     path('api/', include(router.urls)),
     path('login/', AdminLoginView.as_view(), name='admin-login'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token-refresh'),
-    path('dashboard/', DashboardView.as_view(), name='dashboard'),
+    path('dashboard/', DashboardView.as_view(), name='dashboard'),  # Adicionado como path normal
 ]
