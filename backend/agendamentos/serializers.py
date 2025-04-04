edfef74472b0b32e62e71a20ef65cef6e2448537
slvajunior <johnf.foto@gmail.com>
2025-04-04 10:22:53 -0300

    feat: Novo sistema de Login com interface estilo SO, autenticando com JWT

--- backend/agendamentos/serializers.py
+++ backend/agendamentos/serializers.py
@@ -7,4 +7,19 @@ from .models import Agendamento
 class AgendamentoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Agendamento
-        fields = '__all__'
+        fields = [
+            "id",
+            "data_hora",
+            "servico",
+            "cliente",
+            "salao",
+            "status",
+        ]
+        read_only_fields = ["id", "salao", "status"]
+        extra_kwargs = {
+            "data_hora": {"required": True},
+            "servico": {"required": True},
+            "cliente": {"required": True},
+        }
+        # Adiciona validações adicionais, se necessário
+        # def validate_servico(self, value):
